2ccd241c59fb668a1099b004bf67d51ffe1fc83f
Nguyen Viet Bach <nvbach91@outlook.com>
2025-02-21 12:28:54 +0100

    nguv03-cv01

--- www/nguv03/index.php
+++ www/nguv03/index.php
@@ -1,73 +1,109 @@
-<?php 
-class Lab {
-    public function __construct($name, $url, $desc, $active) {
-        $this->name = $name;
-        $this->url = $url;
-        $this->desc = $desc;
-        $this->active = $active;
-    }
+<?php
+
+$name = 'Adam';
+
+$brand = 'BMW'; // "
+$model = 'iX';
+$year = 2024;
+$performance = 200;
+$price = 41234.50;
+$isDiscontinued = false;
+$isLuxuryCar = true;
+// index 0        1          2
+$features = ['sport', 'comfort', 'large wheels'];
+$features[0]; // sport
+$features[1]; // comfort
+$features[2]; // large wheels
+
+class Book
+{
+  public $title;
+  public $price;
+  public function __construct($title, $price)
+  {
+    $this->title = $title;
+    $this->price = $price;
+  }
 }
-function addZeroPadding($num, $length) {
-    while (strlen($num) < $length) {
-        $num = '0'.$num;
-    }
-    return $num;
-}
-$labs = [];
-array_push($labs, new Lab('Lab 01', './cv01/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 02', './cv02/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 03', './cv03/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 04', './cv04/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 05', './cv05/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 06', './cv06/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 07', './cv07/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 08', './cv08/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 09', './cv09/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 10', './cv10/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 11', './cv11/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 12', './cv12/', 'I have no idea what I am doing', true));
-array_push($labs, new Lab('Lab 13', './cv13/', 'I have no idea what I am doing', true));
+
+$myBook = new Book(
+  'Harry Potter and the Chamber of Secrets',
+  500,
+);
+
+$myBook->title; // Harry Potter ...
+$myBook->price; // 500
+
+// associative arrays
+$player = [
+  'name' => 'Ronaldo',
+  'age' => 38,
+  'goals' => 1000,
+];
+$player['name']; // Ronaldo
+$player['age']; // 38
+$player['goals']; // 1000
+
+?>
+
+
+<?php
+
+$firstName = 'David';
+$lastName = 'Beckham';
+$email = 'david@beckham.com';
+$profession = 'Football player';
+$position = 'Midfielder/Winger';
+$salary = 500000;
+$currency = 'EUR';
+$logo = 'https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Real_Madrid_CF.svg/800px-Real_Madrid_CF.svg.png';
+$birthYear = 1975;
+$age = date('Y') - $birthYear;
+$company = 'Real Madrid C.F.';
+$street = 'Avenida de Concha Espina 1';
+$city = 'Madrid';
+$zip = '28036';
+$phone = '+34 91 3984300';
+$web = 'https://www.realmadrid.com';
+$isRetired = true;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>4IZ278 Labs</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-    <link rel="stylesheet" href="./css/styles.css">
-    <style>
-        .card-img-top {
-            width: 50%;
-            margin: auto;
-        }
-        .card {
-            padding: 10px;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
-    <header></header>
-    <main class="container">
-        <h1 class="text-center">My labs</h1>
-        <div class="row labs text-center">
-            <?php foreach ($labs as $lab): ?>
-            <div class="card" style="width: 25%;">
-                <img class="card-img-top" src="http://4.bp.blogspot.com/-ljIAvYh4Vkc/TuddQdJVQHI/AAAAAAAABnY/i1oMO4XbNic/s1600/icon_black.jpg" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo $lab->name; ?></h5>
-                    <p class="card-text"><?php echo $lab->desc; ?></p>
-                    <a class="btn <?php echo $lab->active ? 'btn-primary' : 'btn-secondary'; ?>" href="<?php echo $lab->active ? $lab->url : '#'; ?>"><?php echo $lab->name; ?></a>
-                </div>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </main>
-    <footer></footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
+  <div class="business-card">
+    <div class="bc-firstName"><?php echo $firstName; ?></div>
+    <div class="bc-lastName"><?php echo $lastName; ?></div>
+    <div class="bc-email">
+      <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+    </div>
+    <div class="bc-profession"><?php echo $profession; ?></div>
+    <div class="bc-position"><?php echo $position; ?></div>
+    <div class="bc-salary">Salary: <?php echo $salary; ?><?php echo $currency; ?></div>
+    <div class="bc-logo">
+      <img src="<?php echo $logo; ?>" width="70">
+    </div>
+    <div class="bc-birthYear">Date of birth: <?php echo $birthYear; ?></div>
+    <div class="bc-age">Age: <?php echo $age; ?></div>
+    <div class="bc-company"><?php echo $company; ?></div>
+    <div class="bc-street"><?php echo $street; ?></div>
+    <div class="bc-city">
+      <?php echo $city; ?>, <?php echo $zip; ?>
+    </div>
+    <div class="bc-phone">
+      <a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
+    </div>
+    <div class="bc-web">
+      <a href="<?php echo $web; ?>" target="_blank">Official website</a>
+    </div>
+    <div class="bc-isRetired"><?php echo $isRetired ? 'Retired' : 'Active'; ?></div>
+  </div>
 </body>
+
 </html>
