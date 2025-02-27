bc0e5a0a267f4c65459e179591863f24c0bab7e6
OndrejZemlicka <112846643+OndrejZemlicka@users.noreply.github.com>
2025-02-27 13:12:28 +0100

    Oprava meta řádku v index.php

--- www/zemo00/cv01/index.php
+++ www/zemo00/cv01/index.php
@@ -35,7 +35,6 @@ if ($isLookingForWork){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="1000000">
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
