be965a4e541ad50cd74514c57b95ea1f6927a767
Nguyen Viet Bach <nvbach91@outlook.com>
2025-03-14 10:32:51 +0100

    Revert "enga03-cv03"

--- includes/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Form</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="bg-gray-100 flex flex-col items-center justify-center min-h-screen"></body>
