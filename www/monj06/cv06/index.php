576ff67398639fb1e08c0bfd3e5da8726e8cf09e
mondsjar <129621618+mondsjar@users.noreply.github.com>
2025-03-27 22:53:16 +0100

    monj06-cv06
    Odevzdavam cviceni 06 https://eso.vse.cz/~monj06/4IZ278/cv06/

---
+++ www/monj06/cv06/index.php
@@ -0,0 +1,12 @@
+<?php include __DIR__ . '/includes/header.php'; ?>
+<!-- Page Content-->
+<div class="container">
+    <div class="row">
+        <?php include __DIR__ . '/includes/categoryDisplay.php'; ?>
+        <div class="col-lg-9">
+            <?php include __DIR__ . '/includes/slideDisplay.php'; ?>
+            <?php include __DIR__ . '/includes/productDisplay.php'; ?>
+        </div>
+    </div>
+</div>
+<?php include __DIR__ . '/includes/footer.php'; ?>
