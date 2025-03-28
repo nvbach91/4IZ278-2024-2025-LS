2ee63cc1543843745a5af3c71dfbcd3f198eb798
Nguyen Viet Bach <nvbach91@outlook.com>
2025-03-28 12:47:55 +0100

    added cv07a

---
+++ www/nguv03/cv07a/data.sql
@@ -0,0 +1,249 @@
+DROP TABLE IF EXISTS `players`;
+
+CREATE TABLE `players` (
+  `id` int unsigned NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `image` varchar(255),
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Urielle Gould","http://google.com/en-ca"),
+  ("Gil Schmidt","https://guardian.co.uk/en-us"),
+  ("Aurora Carey","http://google.com/en-ca"),
+  ("Jonah Sanders","https://twitter.com/site"),
+  ("Aidan England","http://guardian.co.uk/fr"),
+  ("Stone Cooke","http://whatsapp.com/one"),
+  ("Ulysses Larsen","http://instagram.com/user/110"),
+  ("Wade Osborne","http://cnn.com/site"),
+  ("Sasha Benjamin","http://whatsapp.com/site"),
+  ("Macey Bradshaw","https://wikipedia.org/fr");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Clinton Mcmahon","https://reddit.com/en-ca"),
+  ("Rafael Nolan","https://naver.com/site"),
+  ("William Price","http://twitter.com/user/110"),
+  ("Cyrus Ray","https://instagram.com/user/110"),
+  ("Germaine Burch","https://google.com/one"),
+  ("Kiara Robinson","https://wikipedia.org/en-ca"),
+  ("Damon Lott","https://ebay.com/sub/cars"),
+  ("Price Knight","http://yahoo.com/one"),
+  ("Rafael Gomez","http://pinterest.com/en-ca"),
+  ("Jerome Buchanan","http://instagram.com/sub/cars");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Emma Henry","https://reddit.com/fr"),
+  ("Phillip Osborne","http://ebay.com/sub"),
+  ("George Reynolds","https://bbc.co.uk/en-ca"),
+  ("Declan Walter","https://zoom.us/en-ca"),
+  ("Ronan Rivas","http://yahoo.com/en-us"),
+  ("Kylee Dalton","https://pinterest.com/site"),
+  ("Kylee Fuller","http://bbc.co.uk/en-ca"),
+  ("Carson Dean","http://baidu.com/sub/cars"),
+  ("Owen Benton","https://walmart.com/sub/cars"),
+  ("Lee Harding","http://guardian.co.uk/site");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Quyn Schneider","http://youtube.com/en-ca"),
+  ("Tate Cooke","https://facebook.com/fr"),
+  ("Thor Nielsen","http://reddit.com/sub"),
+  ("Bruce Russo","http://whatsapp.com/one"),
+  ("Lawrence Mcbride","http://google.com/group/9"),
+  ("Harlan Gillespie","http://netflix.com/sub/cars"),
+  ("Jason Lester","http://pinterest.com/sub"),
+  ("Duncan Bright","http://walmart.com/en-ca"),
+  ("Sharon Black","https://ebay.com/user/110"),
+  ("Austin Holder","https://reddit.com/site");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Moses George","https://cnn.com/en-us"),
+  ("Blossom Pugh","http://yahoo.com/sub/cars"),
+  ("Jack Orr","https://wikipedia.org/group/9"),
+  ("Jerome Solis","https://facebook.com/sub/cars"),
+  ("Carla Duke","https://whatsapp.com/en-ca"),
+  ("Tiger Cantrell","http://pinterest.com/en-us"),
+  ("Hedy Jensen","http://whatsapp.com/user/110"),
+  ("Nigel Brock","http://instagram.com/en-ca"),
+  ("Warren Rush","http://ebay.com/sub/cars"),
+  ("Geoffrey Mccormick","http://nytimes.com/group/9");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Oleg Lancaster","http://zoom.us/site"),
+  ("Avram Donaldson","http://whatsapp.com/group/9"),
+  ("Axel Galloway","http://yahoo.com/group/9"),
+  ("Stuart Koch","https://nytimes.com/site"),
+  ("Bo Mcfarland","https://wikipedia.org/en-us"),
+  ("David Pearson","https://reddit.com/en-us"),
+  ("Clinton Garza","http://facebook.com/fr"),
+  ("Jolie Guthrie","http://instagram.com/user/110"),
+  ("Adrienne Hunter","http://zoom.us/one"),
+  ("Nerea Mckee","https://ebay.com/group/9");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Denton Winters","https://whatsapp.com/sub/cars"),
+  ("Callum Winters","http://twitter.com/group/9"),
+  ("Mia Franklin","http://facebook.com/en-us"),
+  ("Barbara Medina","https://youtube.com/en-ca"),
+  ("Kathleen Goodman","https://netflix.com/site"),
+  ("Anthony Galloway","https://cnn.com/en-ca"),
+  ("Chase Marks","http://yahoo.com/one"),
+  ("Jakeem Harrington","https://youtube.com/en-ca"),
+  ("Neil Ramos","http://ebay.com/one"),
+  ("Rose Barrett","http://guardian.co.uk/user/110");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Patience Alvarado","http://bbc.co.uk/site"),
+  ("Zelda Savage","http://google.com/sub"),
+  ("Nora Rodgers","https://nytimes.com/en-us"),
+  ("Ronan Trujillo","https://bbc.co.uk/en-ca"),
+  ("Victor Reed","https://ebay.com/sub"),
+  ("Ferris Hawkins","http://reddit.com/site"),
+  ("Hoyt Mayer","http://guardian.co.uk/site"),
+  ("Peter Camacho","http://facebook.com/fr"),
+  ("Tara Bolton","http://netflix.com/sub"),
+  ("Brianna Drake","http://twitter.com/one");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Kelly Franco","http://baidu.com/user/110"),
+  ("Josephine Duffy","https://nytimes.com/settings"),
+  ("Marvin Cook","http://bbc.co.uk/site"),
+  ("August Steele","https://whatsapp.com/en-us"),
+  ("Erica Henry","https://whatsapp.com/group/9"),
+  ("Zenia Espinoza","http://whatsapp.com/settings"),
+  ("Quon Alvarez","http://pinterest.com/user/110"),
+  ("Oleg Tillman","http://pinterest.com/en-us"),
+  ("Hector Ratliff","https://google.com/user/110"),
+  ("Jasmine Harper","https://naver.com/en-us");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Cassady Hammond","https://facebook.com/sub/cars"),
+  ("Lila Hughes","http://ebay.com/fr"),
+  ("Mohammad Ewing","http://netflix.com/group/9"),
+  ("Yvonne Wilkerson","http://netflix.com/sub"),
+  ("Olympia Delgado","https://nytimes.com/user/110"),
+  ("Neil Wilson","https://facebook.com/sub/cars"),
+  ("Deacon Mathews","http://zoom.us/group/9"),
+  ("Murphy Burke","https://bbc.co.uk/en-ca"),
+  ("Colleen Hess","https://youtube.com/sub/cars"),
+  ("Ramona Villarreal","https://nytimes.com/fr");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Aurora Underwood","https://reddit.com/site"),
+  ("Tarik Chaney","https://nytimes.com/settings"),
+  ("Sawyer Long","http://wikipedia.org/user/110"),
+  ("Randall Baxter","http://netflix.com/group/9"),
+  ("Selma Mason","http://cnn.com/group/9"),
+  ("Gabriel Soto","https://reddit.com/en-us"),
+  ("Tanek Meadows","https://zoom.us/fr"),
+  ("Minerva Pearson","http://baidu.com/fr"),
+  ("Nicole Mendoza","http://walmart.com/en-us"),
+  ("Richard Hays","https://zoom.us/settings");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Tamekah Avery","http://facebook.com/sub"),
+  ("Hedy Young","https://yahoo.com/en-us"),
+  ("Brock Erickson","https://twitter.com/en-us"),
+  ("Lael Lang","http://youtube.com/sub/cars"),
+  ("Nevada Cochran","https://naver.com/sub/cars"),
+  ("Allegra Kirby","http://nytimes.com/fr"),
+  ("Iliana Neal","http://youtube.com/one"),
+  ("Jelani Burgess","http://baidu.com/site"),
+  ("Wallace Cortez","https://youtube.com/sub"),
+  ("Maryam Sanders","http://bbc.co.uk/en-ca");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Yetta Forbes","https://google.com/group/9"),
+  ("Laurel Baxter","http://zoom.us/sub/cars"),
+  ("Yael Hebert","https://naver.com/site"),
+  ("Pascale Everett","http://guardian.co.uk/site"),
+  ("Beverly York","http://whatsapp.com/site"),
+  ("Phelan Clements","http://zoom.us/sub"),
+  ("McKenzie Fisher","https://whatsapp.com/user/110"),
+  ("Jennifer Mcdonald","https://reddit.com/en-ca"),
+  ("Summer Ware","http://wikipedia.org/settings"),
+  ("Colette Williamson","http://reddit.com/user/110");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Akeem Joyner","https://baidu.com/one"),
+  ("Eve Keller","http://facebook.com/sub/cars"),
+  ("Armando Leach","https://google.com/settings"),
+  ("Jenna Foley","https://cnn.com/user/110"),
+  ("Rosalyn Francis","http://google.com/sub"),
+  ("Vielka Bernard","https://baidu.com/site"),
+  ("Duncan Acosta","http://netflix.com/en-ca"),
+  ("Cassady Lynch","http://instagram.com/user/110"),
+  ("Geoffrey Franklin","http://google.com/en-ca"),
+  ("Yoshi Mcmahon","https://zoom.us/fr");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Guinevere Gutierrez","http://whatsapp.com/en-ca"),
+  ("Nash Whitfield","http://baidu.com/fr"),
+  ("Malcolm Parrish","https://facebook.com/settings"),
+  ("Bo Schroeder","http://naver.com/one"),
+  ("Doris Buckley","https://guardian.co.uk/group/9"),
+  ("Katell Burgess","https://whatsapp.com/en-us"),
+  ("Merritt Harvey","http://bbc.co.uk/group/9"),
+  ("Joshua Bean","http://facebook.com/en-us"),
+  ("Finn Hess","http://google.com/one"),
+  ("Regan May","https://walmart.com/sub");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Tucker O'brien","http://ebay.com/en-us"),
+  ("Stephanie Medina","https://baidu.com/en-us"),
+  ("Libby Wilcox","https://cnn.com/settings"),
+  ("Len Peters","https://bbc.co.uk/user/110"),
+  ("Tyrone Lynn","http://bbc.co.uk/en-ca"),
+  ("Dane Jackson","http://twitter.com/sub"),
+  ("Basia Willis","https://zoom.us/en-us"),
+  ("Kylie Miles","https://baidu.com/site"),
+  ("Leonard Bonner","https://walmart.com/sub"),
+  ("Hedda Barnett","https://pinterest.com/en-us");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Elaine Hebert","https://naver.com/fr"),
+  ("Quemby Bright","http://ebay.com/user/110"),
+  ("Curran Sanford","https://bbc.co.uk/one"),
+  ("Lee Short","https://pinterest.com/sub/cars"),
+  ("Lucy Bell","http://youtube.com/one"),
+  ("Lucius Mcgowan","http://twitter.com/en-us"),
+  ("Ray Russo","http://wikipedia.org/settings"),
+  ("Lee Anthony","https://reddit.com/settings"),
+  ("Giacomo Ingram","http://naver.com/group/9"),
+  ("Coby Bradley","https://facebook.com/settings");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("McKenzie Wiggins","http://reddit.com/en-us"),
+  ("Mannix Britt","http://twitter.com/sub"),
+  ("Lacy Holloway","https://facebook.com/en-ca"),
+  ("Autumn Hardy","https://netflix.com/user/110"),
+  ("Levi Ellis","http://cnn.com/settings"),
+  ("Lucian Justice","http://pinterest.com/site"),
+  ("Tanner Richard","https://bbc.co.uk/en-ca"),
+  ("Clark Hancock","https://zoom.us/fr"),
+  ("Nora Briggs","https://bbc.co.uk/site"),
+  ("Jakeem Wiggins","http://google.com/group/9");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Dai Sanders","http://facebook.com/en-us"),
+  ("Ramona Kinney","http://zoom.us/settings"),
+  ("Thor Meyers","http://cnn.com/one"),
+  ("Rhona Farley","https://ebay.com/sub"),
+  ("Brenden Burke","https://youtube.com/sub/cars"),
+  ("Simone Boyd","http://facebook.com/user/110"),
+  ("Lewis Larson","http://netflix.com/en-ca"),
+  ("Francis Kirkland","https://nytimes.com/site"),
+  ("Whitney Oneal","http://baidu.com/en-us"),
+  ("Nigel Frazier","https://cnn.com/en-ca");
+INSERT INTO `players` (`name`,`image`)
+VALUES
+  ("Emi Simpson","https://whatsapp.com/sub"),
+  ("Abdul Sheppard","https://ebay.com/user/110"),
+  ("Plato Perkins","http://google.com/sub/cars"),
+  ("Wayne Neal","https://instagram.com/user/110"),
+  ("Eaton Travis","http://google.com/settings"),
+  ("Solomon Roman","http://yahoo.com/settings"),
+  ("Cody Morton","https://nytimes.com/fr"),
+  ("Renee Alexander","http://nytimes.com/user/110"),
+  ("Ocean Cash","https://ebay.com/fr"),
+  ("Wyoming Wise","https://ebay.com/one");
