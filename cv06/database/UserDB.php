978cac39471232976966c9446313c1408930788f
kacenkacicimnau <116424807+kacenkacicimnau@users.noreply.github.com>
2025-03-28 12:23:24 +0100

    Delete cv06 directory

--- cv06/database/UserDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/database.php';
-
-class UserDB extends Database {
-    protected $tableName = 'users';
-
-    public function resetPassword($args) {}
-    public function changeAvatar($args) {}
-}
--- cv06/database/database-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-const DB_SERVER_URL = "127.0.0.1";
-const DB_USERNAME = "";
-const DB_PASSWORD = "";
-const DB_DATABASE = "kark28;";
-
-?>
