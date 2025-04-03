b1d713b3eebf874a7f0992c7eeccc54dccf9bfa7
Amelie Engelmaierová <amelie.engelmaier@gmail.com>
2025-04-04 00:40:09 +0200

    oop leak

--- www/enga03/cv07/database/database-config.php
+++ www/enga03/cv07/database/database-config.php
@@ -1,11 +1,7 @@
 <?php
-const DB_SERVER_URL = 'localhost';
-const DB_USERNAME = 'enga03';
-const DB_PASSWORD = 'xahKohpahxaixei3ie';
-const DB_DATABASE = 'enga03';
-// const DB_SERVER_URL = '127.0.0.1';
-// const DB_USERNAME = 'root';
-// const DB_PASSWORD = '';
-// const DB_DATABASE = 'cv07';
+const DB_SERVER_URL = '';
+const DB_USERNAME = '';
+const DB_PASSWORD = '';
+const DB_DATABASE = '';
 
 ?>
