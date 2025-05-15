9e4bc1aa7bae3f3ebf3f44308344fe497c20983c
OndrejZemlicka <112846643+OndrejZemlicka@users.noreply.github.com>
2025-05-16 00:25:41 +0200

    zemo00-cv12
    Odevzdávám cvičení č. 12
    
    Odkaz: https://eso.vse.cz/~zemo00/cv12/app/todo

---
+++ www/zemo00/cv12/laravel/database/migrations/2025_05_09_102316_create_todos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todos', function (Blueprint $table) {
+        $table->id();                // column primary key
+        $table->timestamps();        // column created_at, updated_at
+        $table->string('title');     // column title - text
+        $table->boolean('finished'); // column finished - 0 / 1
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todos');
+    }
+};
