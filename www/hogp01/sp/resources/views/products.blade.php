6c54f9e49a457447626debfbadbc819aee33613f
Peter Hogya <peter@hogy.eu>
2025-05-16 19:12:50 +0200

    hogp01-sp

---
+++ www/hogp01/sp/resources/views/products.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jmeno Eshopu</title>
+  <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+
+</head>
+<body>
+  <x-navbar></x-navbar>
+
+
+  <x-footer></x-footer>
+
+</body>
+</html>
