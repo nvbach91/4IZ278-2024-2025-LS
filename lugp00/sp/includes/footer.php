4302abb2ba46a867ee46debdc9c4d57f94c5d384
Nguyen Viet Bach <nvbach91@outlook.com>
2025-06-20 12:43:10 +0200

    remove duplicate folder

--- lugp00/sp/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-// sp/includes/footer.php
-// Uzavře kontejner, připojí JS a uzavře dokument.
-
-?>
-</div><!-- /.container -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
